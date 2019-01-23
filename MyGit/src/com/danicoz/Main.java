3aa239b381e7783935311a7a79b658aec3ebaa05
Danicoz <1421537902@qq.com>
2019-01-23 15:40:41 +0800

    Update Main.java

--- MyGit/src/com/danicoz/Main.java
+++ MyGit/src/com/danicoz/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("测试2：远程修改代码");
 		System.out.println("测试4：develop merge master");
 		System.out.println("测试5：master merge develop");
+		System.out.println("测试6：master merge develop");
 
 	}
 }
