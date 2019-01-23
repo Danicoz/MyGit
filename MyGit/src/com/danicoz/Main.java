f956a4c98785f3673e08538098009e95cc2e5d7c
刘永坚 <1421537902@qq.com>
2019-01-23 15:09:35 +0800

    冲突代码

--- MyGit/src/com/danicoz/Main.java
+++ MyGit/src/com/danicoz/Main.java
@@ -3,6 +3,7 @@ package com.danicoz;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("测试1");
+		System.out.println("测试3：冲突解决");
 		System.out.println("测试2：远程修改代码");
 
 	}
