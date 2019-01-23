19889439028895154c8783e72587720788db88bc
Danicoz <1421537902@qq.com>
2019-01-23 17:30:46 +0800

    Update Main.java

--- MyGit/src/com/danicoz/Main.java
+++ MyGit/src/com/danicoz/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		System.out.println("测试5：master merge develop");
 		System.out.println("测试6：master merge develop");
 		System.out.println("测试7：develop merge master");
+		System.out.println("测试8：master merge develop");
 
 	}
 }
